74309fc15833b3e3ef38a8b69725d28d3f32c104
Ethan <ethangbraun@gmail.com>
2022-08-07 22:20:08 -0500

    Created a program to demonstrate queues and queue sets

--- FreeRTOS/Demo/Posix_GCC/main_user.c
+++ FreeRTOS/Demo/Posix_GCC/main_user.c
@@ -12,35 +12,96 @@
 /* Local includes. */
 #include "console.h"
 
-QueueHandle_t myQueue;
+#define MS 50
+
+typedef enum {
+    MALE,
+    FEMALE,
+    NONBINARY
+} sex;
+
+typedef struct User{
+    char* m_name;
+    int16_t m_age;
+    sex m_sex;
+} User;
+
+QueueHandle_t myQueue1;
+QueueHandle_t myQueue2;
+
+QueueSetHandle_t myQueueSet;
+
 void queueReceiveTask(void *params);
-void queueSendTask(void *params);
+void queueSendTask1(void *params);
+void queueSendTask2(void *params);
 
 void main_mine( void ) {
+    BaseType_t status;
 
-    myQueue = xQueueCreate(2, sizeof(int32_t));
+    User me = {"Ethan", 21, MALE };
+    User mom = {"Kira", 46, FEMALE };
+    User dad = {"Jason", 52, MALE };
+    User Izz = {"Izz", 20, NONBINARY};
 
-    if (myQueue == NULL)
-        printf("Memory could not be allocated for queue");
+    myQueue1 = xQueueCreate(5, sizeof(User));
+    myQueue2 = xQueueCreate(5, sizeof(User));
+    if (myQueue1 == NULL)
+        printf("Memory could not be allocated for queue 1\n");
 
-    xTaskCreate(queueSendTask, "Send 1", 1000, "The monkey", 1, NULL);
-    xTaskCreate(queueSendTask, "Send 2", 1000, "The chicken", 1, NULL);
+    if (myQueue2 == NULL)
+        printf("Memory could not be allocated for queue 2\n");
 
-    xTaskCreate(queueReceiveTask, "Receive", 1000, NULL, 2, NULL);
+    myQueueSet = xQueueCreateSet(10);
+    if (myQueueSet == NULL)
+        printf("Queue set was not created successfully\n");
+    
+    status = xQueueAddToSet(myQueue1, myQueueSet);
+    if(status == pdFAIL)
+        printf("Error adding queue 1 to set\n");
+    
+    status = xQueueAddToSet(myQueue2, myQueueSet);
+    if(status == pdFAIL)
+        printf("Error adding queue 2 to set\n");
+
+
+    xTaskCreate(queueSendTask1, "Send 1", 1000, &me, 1, NULL);
+    xTaskCreate(queueSendTask2, "Send 2", 1000, &mom, 1, NULL);
+
+    xTaskCreate(queueReceiveTask, "Receive", 1000, NULL, 1, NULL);
     vTaskStartScheduler();
 
     for(;;);
 }
 
-void queueSendTask (void *params){
-    char* s = (char*)pvPortMalloc(32*sizeof(char));
-    strcpy(s, (char*)params);
+void queueSendTask1 (void *params){
+    User *currentUser = (User*) params;
+    TickType_t ticksToWait = pdMS_TO_TICKS(MS);
     //printf("queueSendTask received the following string: %s\n", s);
 
     BaseType_t status;
 
     for (;;){
-        status = xQueueSend(myQueue, &s, 0);
+        vTaskDelay(ticksToWait);
+
+        status = xQueueSend(myQueue1, &currentUser, 0);
+        //vTaskDelay(pdMS_TO_TICKS(100));
+
+        if (status != pdPASS)
+            printf("Not able to send to queue\n");
+    }
+}
+
+void queueSendTask2 (void *params){
+    User *currentUser = (User*) params;
+    TickType_t ticksToWait = pdMS_TO_TICKS(MS);
+    //printf("queueSendTask received the following string: %s\n", s);
+
+    BaseType_t status;
+
+    for (;;){
+        vTaskDelay(ticksToWait * 4);
+
+        status = xQueueSend(myQueue2, &currentUser, 0);
         //vTaskDelay(pdMS_TO_TICKS(100));
 
         if (status != pdPASS)
@@ -49,20 +110,37 @@ void queueSendTask (void *params){
 }
 
 void queueReceiveTask(void *params){
-    char* buffer;
+    User* currentUser;
     BaseType_t status;
-    TickType_t ticksToWait = pdMS_TO_TICKS(100);
+    char* userSex;
+    QueueHandle_t currentQueue;
+    TickType_t ticksToWait = pdMS_TO_TICKS(MS);
 
     for(;;){
-        if (uxQueueMessagesWaiting(myQueue) != 0)
-            printf("Queue should have been empty\n");
-        
-        status = xQueueReceive(myQueue, &buffer, ticksToWait);
+        currentQueue = (QueueHandle_t) xQueueSelectFromSet(myQueueSet, 0);
+        if (currentQueue == NULL){
+            //printf("Could not select queue\n"); 
+            continue;
+        }
+        status = xQueueReceive(currentQueue, &currentUser, 0);
 
         if (status != pdPASS)
             printf("Unable to pull from queue");
-        else
-            printf("Received string: %s\n", buffer);
+        else{
+            switch (currentUser->m_sex){
+                case MALE:
+                    userSex = "Male";
+                    break;
+                case FEMALE:
+                    userSex = "Female";
+                    break;
+                case NONBINARY:
+                    userSex = "Non-binary";
+                    break;
+            }
+
+            printf("%s is %d years old and %s\n",currentUser->m_name,currentUser->m_age,userSex);
+        }
 
         //vPortFree(buffer);
     }
